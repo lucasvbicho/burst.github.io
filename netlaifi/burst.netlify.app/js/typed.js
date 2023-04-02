8769832d7824251477ab1e17285a975269442da1
lucasvbicho <108143656+lucasvbicho@users.noreply.github.com>
2023-04-02 23:10:21 +0400

    Add files via upload

---
+++ netlaifi/burst.netlify.app/js/typed.js
@@ -0,0 +1,7 @@
+new Typed("#typed", {
+    strings: [, "burst owns u", ".gg/naVjJspDnY", "smh", "best pussy licker ww", "stop stalking me nigga", "blood walkin on yo body"],
+    typeSpeed: 20,
+    delaySpeed: 90,
+    showCursor: !1,
+    loop: !0
+});
